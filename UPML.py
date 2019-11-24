c04b48bcceddec756f9c1c91995de242eb7ecdbd
Dmitry Ponyatov <dponyatov@gmail.com>
2019-11-24 06:40:28 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- UPML.py
+++ UPML.py
@@ -1,36 +1,4 @@
 
-
-    def pop(self): return self.nest.pop(-1)
-    def top(self): return self.nest[-1]
-
-class Primitive(Frame): pass
-class Symbol(Primitive): pass
-class String(Primitive): pass
-
-class Active(Frame): pass
-class VM(Active): pass
-class Cmd(Active):
-    def __init__(self, F):
-        Active.__init__(self,F.__name__)
-        self.fn = F
-    def eval(self,ctx):
-        self.fn(ctx)
-
-vm = VM('UPML') ; vm << vm
-
-import ply.lex  as lex
-
-tokens = ['symbol']
-
-t_ignore         = ' \t\r\n'
-t_ignore_comment = r'[\#\\].*'
-
-def t_symbol(t):
-    r'[`]|[^ \t\r\n\#\\]+'
-    return Symbol(t.value)
-
-def t_error(t): raise SyntaxError(t)
-
 def WORD(ctx):
     token = ctx.lexer.token()
     if token: ctx // token
